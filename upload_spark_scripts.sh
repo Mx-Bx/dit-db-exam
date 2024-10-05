9c189402debf2a767d6f3a467beedf1bcee6782a
Mx-Bx <mxmbx22@gmail.com>
2024-10-05 11:51:59 +0000

    add some scripts

--- upload_spark_scripts.sh
+++ upload_spark_scripts.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-kubectl delete job.batch/copy-spark-script
-kubectl delete configmap spark-script-config
+#kubectl delete job.batch/copy-spark-script
+#kubectl delete configmap spark-script-config
 
-kubectl create configmap spark-script-config --from-file=/home/barryma/Workspace/tasks/kubernetes/examen-db/src/spark/scripts/ --namespace data-pipeline
+#kubectl create configmap spark-script-config --from-file=/home/barryma/Workspace/tasks/kubernetes/examen-db/src/spark/scripts/ --namespace data-pipeline
+kubectl cp /home/barryma/Workspace/tasks/kubernetes/examen-db/src/spark/scripts/spark_streaming_ecommerce.py test-pvc-pod:/mnt/data/spark-data/upload-dir/spark_streaming_ecommerce.py
 
-kubectl apply -f src/spark/spark-script-copy-job.yaml
+kubectl cp /home/barryma/Workspace/tasks/kubernetes/examen-db/src/spark/scripts/spark_streaming.py test-pvc-pod:/mnt/data/spark-data/upload-dir/spark_streaming.py
+
+
+#kubectl apply -f src/spark/spark-script-copy-job.yaml
